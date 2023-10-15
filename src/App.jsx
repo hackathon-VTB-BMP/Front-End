63f0d2a677e8106416da66138666a0f8a8375d4c
Alla Artem <artemav.3701@gmail.com>
2023-10-15 09:28:05 +0700

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import axios from "axios";
 import "./App.css";
-import { useState } from "react";
-import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
+import { useState, useRef, useEffect } from "react";
+import { YMaps, Map, Placemark, Clusterer } from "@pbe/react-yandex-maps";
 import { useQuery } from "react-query";
 import { useDisclosure } from "@chakra-ui/react";
-import DrawerComponent from "./components/DrawerComponent";
+import DrawerComponent from "./components/drawer-components/DrawerComponent";
+import Header from "./components/header/Header";
 
 function App() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [dataProps, setDataProps] = useState();
+
+  const [ymaps, setYmaps] = useState(null);
+  const map = useRef(null);
+  const [userLocation, setUserLocation] = useState(null);
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        setUserLocation([latitude, longitude]);
+      });
+    } else {
+      console.error("Geolocation is not supported by your browser");
+    }
+  }, []);
 
   const getOffices = async () => {
     const data = await axios.get("http://localhost:3000/office");
@@ -22,36 +38,71 @@ function App() {
     setDataProps(marker);
   };
 
+  const addRoute = (myPoint, pointB) => {
+    const multiRoute = new ymaps.multiRouter.MultiRoute(
+      {
+        referencePoints: [myPoint, pointB],
+        params: {
+          routingMode: "pedestrian",
+        },
+      },
+      {
+        boundsAutoApply: true,
+      }
+    );
+
+    map.current.geoObjects.add(multiRoute);
+  };
+
   return (
-    <YMaps
-      query={{ lang: "en_RU", apikey: "53b6fdca-2776-4953-9525-5ba28ea6ea85" }}
-    >
-      <Map
-        width="100%"
-        height="99.9vh"
-        defaultState={{ center: [55.75, 37.57], zoom: 9 }}
+    <>
+      <Header />
+      <YMaps
+        query={{
+          lang: "en_RU",
+          apikey: "53b6fdca-2776-4953-9525-5ba28ea6ea85",
+        }}
       >
-        {data &&
-          data.map((marker) => (
-            <Placemark
-              options={{
-                iconLayout: "default#image",
-                iconImageHref: "images/VTB_icon.svg",
-                iconImageSize: [70, 70],
-                iconImageOffset: [-20, -40],
-              }}
-              key={marker.salePointName}
-              geometry={[marker.latitude, marker.longitude]}
-              onClick={() => handleClick(marker)}
-            />
-          ))}
-        <DrawerComponent
-          onClose={onClose}
-          isOpen={isOpen}
-          dataProps={dataProps}
-        />
-      </Map>
-    </YMaps>
+        <Map
+          width="100%"
+          height="99.9vh"
+          defaultState={{ center: [55.75, 37.57], zoom: 9 }}
+          modules={["multiRouter.MultiRoute"]}
+          onLoad={(ymap) => setYmaps(ymap)}
+          instanceRef={map}
+        >
+          <Clusterer
+            options={{
+              preset: "islands#darkBlueCircleIcon",
+              groupByCoordinates: false,
+            }}
+          >
+            {data &&
+              data.map((marker) => (
+                <Placemark
+                  options={{
+                    iconLayout: "default#image",
+                    iconImageHref: "images/VTB_icon.svg",
+                    iconImageSize: [70, 70],
+                    iconImageOffset: [-20, -40],
+                  }}
+                  key={marker.salePointName}
+                  geometry={[marker.latitude, marker.longitude]}
+                  onClick={() => handleClick(marker)}
+                />
+              ))}
+          </Clusterer>
+
+          <DrawerComponent
+            onClose={onClose}
+            isOpen={isOpen}
+            dataProps={dataProps}
+            addRoute={addRoute}
+            userLocation={userLocation}
+          />
+        </Map>
+      </YMaps>
+    </>
   );
 }
 
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,50 @@
+import styles from "./header.module.css";
+import { useState } from "react";
+import { Menu, MenuButton, MenuList, MenuItem, Button } from "@chakra-ui/react";
+import { ChevronUp, ChevronDown, HelpCircle } from "lucide-react";
+// import logo from "../../img/logo.png";
+
+const Header = () => {
+  const [openInfo, setOpenInfo] = useState(false);
+
+  return (
+    <div className={styles.header}>
+      {/* <div className={styles.top_header}>
+        <img src={logo} alt="logo" />
+      </div> */}
+      <div className={styles.bott_header}>
+        <div className={styles.title}>Лучший офис в удобном месте</div>
+        <div className={styles.buttons_wrapper}>
+          <div className={styles.menu}>
+            <Menu>
+              <MenuButton as={Button} onClick={() => setOpenInfo(!openInfo)}>
+                <div style={{ display: "flex", gap: "10px" }}>
+                  <span>Сортировка по</span>{" "}
+                  {openInfo ? <ChevronUp /> : <ChevronDown />}
+                </div>
+              </MenuButton>
+              <MenuList>
+                <MenuItem>Сначала менее загруженные</MenuItem>
+                <MenuItem>Сначала ближайшие</MenuItem>
+              </MenuList>
+            </Menu>
+          </div>
+          <button className={styles.quiz}>
+            <div
+              style={{
+                display: "flex",
+                gap: "10px",
+                alignItems: "center",
+                justifyContent: "space-around",
+              }}
+            >
+              <span>Помощь с выбором отделения</span> <HelpCircle size="24px" />
+            </div>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
